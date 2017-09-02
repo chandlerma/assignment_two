bff23c9545cd2f9b102129b4056890608c78a22e
chandlerma <31550078+chandlerma@users.noreply.github.com>
2017-09-01 21:28:24 -0400

    Update assignment2.java

--- src/assignment2.java
+++ src/assignment2.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 public class assignment2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double monthlyS;
+        double balance = 0;
         int i;
 
         System.out.println("please enter your monthly saving amount");
-        monthlyS = scan.nextDouble();
+        double monthlyS = scan.nextDouble();
 
-        for (i = 1; i <6; i++){
-            monthlyS = monthlyS * (1 + 0.00417) ;
-            System.out.println(monthlyS);
+        for (i = 1; i <7; i++){
+            balance = (balance + monthlyS) * (1 + 0.00417) ;
         }
+        System.out.println("your saving after 6 months will be " + balance);
     }
 }
